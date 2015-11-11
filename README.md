7344257c9cc0c9c06fc3bf39aa2e041be021099e
Lauricio Su <me@lauricio.com>
2015-11-11 16:10:23 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Wrapper for node-gcm
---
+++ package.js
@@ -0,0 +1,33 @@
+Package.describe({
+  name: 'tapfuse:node-gcm',
+  version: '0.12.0_1',
+  // Brief, one-line summary of the package.
+  summary: 'Wrapper for node-gcm',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+var C = 'client';
+var S = 'server';
+var CS = [C, S];
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+
+  Npm.depends({
+    "node-gcm": "0.12.0"
+  });
+
+  api.addFiles('lib/tapfuse-node-gcm.js', S);
+  api.export('GCM', S);
+
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('tapfuse:node-gcm');
+  api.addFiles('tests/package-tests.js');
+});
